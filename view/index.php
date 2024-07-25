f2f31c625b477106f07617674b8f3c6c72632b8d
Stephen Stha <stephencrestha07@gmail.com>
2024-07-25 15:53:15 +0545

    second

---
+++ view/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bus ticketing system</title>
+
+    <!-- Font -->
+     
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@0;1&display=swap" rel="stylesheet">
+
+<!-- Style -->
+     <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+    <header>
+        <div class="info">
+          <div class="left-info">
+            <ul>
+              <li>Welcome</li>
+              <li><a href="tel:9863021927"></a>9841234567</li>
+              <li><a href="mailto:stephencrestha07@gmail.com">info@busticketing.com</a>
+            </ul>
+          </div>
+          <div class="right-info">
+           <ul>
+            
+              <li><a href="#">Home</a></li>         
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Service</a></li>
+              <li><a href="#">Help</a></li>
+            
+            
+           </ul>
+          </div>
+        </div>
+      
+      
+        <nav class="cf">
+          <div class="branding">Bus Ticketing</div>
+
+          
+          <div class="search">
+            <input type="text" placeholder="Search">
+          </div> 
+          
+          
+          <div class=logPage>
+          <button >Login</button>
+          <button >Regsiter</button>
+          </div>
+                        
+        </nav>  
+
+   
+</body>
+</html>
